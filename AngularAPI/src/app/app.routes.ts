04378634ce70ec7958509ef35361cc57b9039f6f
Marcos Sandá <marcos92sanda@gmail.com>
2025-09-18 18:57:53 +0200

    Maquetamos navbar y fijamos rutas

--- AngularAPI/src/app/app.routes.ts
+++ AngularAPI/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'usuario/:id', component: UserViewComponent },
+  { path: 'nuevo-usuario', component: UserFormComponent },
+  { path: 'actualizar-usuario/:id', component: UserFormComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
