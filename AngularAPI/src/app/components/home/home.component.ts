abd478667bd826af2f710ee2e0d26523bc10705b
Marcos Sandá <marcos92sanda@gmail.com>
2025-09-18 20:56:49 +0200

    Actualizamos Home.ts

--- AngularAPI/src/app/components/home/home.component.ts
+++ AngularAPI/src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IUsers } from '../../interfaces/iusers';
+import { Users } from '../../services/users';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  // arrUsuarios: IUsers[] = []
+  // usersServices = inject(Users);
+// 
+  // ngOnInit() {
+  //   this.cargarData()
+  // }
+// 
+  // async cargarData() {
+  //   // Lleno el array de empleados llamando al servicio y pidiendole todos los ampleados: getAll()
+  //   try {
+  //     const response = await this.usersServices.getAll();
+  //     console.log(response)
+  //     this.arrUsuarios = response;
+  //   }
+  //   catch (msg: any) {
+  //     alert(msg.error.error)
+  //   }
+  // }
+// 
+  // getAlertDelete(event: string) {
+  //   this.cargarData()
+  //   // Si quiero puedo añadir un mensaje de aviso con TOAST
+  //   toast.error(event)
+  // }
 }
