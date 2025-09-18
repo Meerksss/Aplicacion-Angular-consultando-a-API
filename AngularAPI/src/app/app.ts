814b8109e80d2c9cb53e185e119811089225c75f
Marcos Sandá <marcos92sanda@gmail.com>
2025-09-18 19:04:45 +0200

    Instalamos y configuramos ngx-sonner

--- AngularAPI/src/app/app.ts
+++ AngularAPI/src/app/app.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { toast, NgxSonnerToaster } from 'ngx-sonner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, NgxSonnerToaster],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'AngularAPI';
+
+    protected readonly toast = toast;
+
 }
